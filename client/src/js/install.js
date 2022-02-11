d502c59415df69875d5fbff398e18e6bbb88624c
Emily Burger <eburger939@gmail.com>
2022-02-11 13:36:41 -0600

    added event listener before install prompt

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,7 +3,10 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    
+    //store event
+    window.deferredPrompt = event;
+    //remove hidden class
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
